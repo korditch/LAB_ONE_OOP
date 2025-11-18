6541ea80590cc3c7577bfcbec9888bbeda290bc6
greylori <greylori@student.21-school.ru>
2025-11-18 15:10:56 +0300

    'first'

---
+++ LR_1/LR_ONE_2/LR_ONE_2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            if (!File.Exists("input.txt"))
+            {
+                File.WriteAllText("input.txt", "10\n20\n10000\n40\n50");
+            }
+
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"output.txt");
+            var new_in = new StreamReader(@"input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+
+            double a, b, c, d, e;
+            double s = 0, k = 0;
+            bool s_calculated = false;
+            bool k_calculated = false;
+
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+            d = Convert.ToDouble(Console.ReadLine());
+            e = Convert.ToDouble(Console.ReadLine());
+
+            if (a < 0 || a > 100000 || b < 0 || b > 100000 ||
+                c < 0 || c > 100000 || d < 0 || d > 100000 ||
+                e < 0 || e > 100000)
+            {
+                Console.WriteLine("ERROR");
+            }
+            else
+            {
+                
+                if ((e - b >= 0) && (a - Math.Sqrt(e - b) >= 0) && (e != 0))
+                {
+                    s = Math.Sqrt(a - Math.Sqrt(e - b)) / e;
+                    Console.WriteLine(String.Format("{0:0.00}", s));
+                }
+                else
+                {
+                    Console.WriteLine("ERROR");
+                }
+
+                if ((c - 1000 >= 0) && (Math.Abs(b - a) != 0) && ((d + Math.Sqrt(c - 1000)) >= 0))
+                {
+                    k = Math.Sqrt((d + Math.Sqrt(c - 1000)) / Math.Abs(b - a));
+                    Console.WriteLine(String.Format("{0:0.00}", k));
+                    k_calculated = true;
+                }
+                else
+                {
+                    Console.WriteLine("ERROR");
+                    
+      
+                }
+            }
+
+            Console.SetOut(save_out);
+            new_out.Close();
+            Console.SetIn(save_in);
+            new_in.Close();
+        }
+    }
+}
---
+++ LR_1/LR_ONE_3/LAB_ONE_3/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+
+namespace LAB_ONE_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!File.Exists("input.txt"))
+            {
+                File.WriteAllText("input.txt", "2\n3\n5\n0");
+            }
+
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+
+            var new_out = new StreamWriter(@"output.txt");
+            var new_in = new StreamReader(@"input.txt");
+
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+
+            double X, Y, Z;
+            int t, N;
+            Z = 0;
+
+            X = Convert.ToDouble(Console.ReadLine());
+            Y = Convert.ToDouble(Console.ReadLine());
+            N = Convert.ToInt32(Console.ReadLine());
+            t = Convert.ToInt32(Console.ReadLine());
+
+            switch (t)
+            {
+                case 0:
+                    ForCycle(X, Y, N, Z);
+                    break;
+                case 1:
+                    WhileCycle(X, Y, N, Z);
+                    break;
+                case 2:
+                    doWhileCycle(X, Y, N, Z);
+                    break;
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+
+            Console.SetOut(save_out);
+            Console.SetIn(save_in);
+            new_out.Close();
+            new_in.Close();
+        }
+
+        static void ForCycle(double X, double Y, int N, double Z)
+        {
+            for (int i = 1; i <= N; i++)
+            {
+                double term;
+                double denominator = (2 * i - 1) * (2 * i + 1);
+
+                if (i % 2 != 0)
+                {
+                    term = Math.Pow(X, 2 * i) / denominator;
+                }
+                else
+                {
+                    term = -Math.Pow(Y, 2 * i) / denominator;
+                }
+
+                Z += term;
+            }
+            Console.WriteLine(String.Format("{0:0.0000}", Z));
+        }
+
+        static void WhileCycle(double X, double Y, int N, double Z)
+        {
+            int i = 1;
+            while (i <= N)
+            {
+                double term;
+                double denominator = (2 * i - 1) * (2 * i + 1);
+
+                if (i % 2 != 0)
+                {
+                    term = Math.Pow(X, 2 * i) / denominator;
+                }
+                else
+                {
+                    term = -Math.Pow(Y, 2 * i) / denominator;
+                }
+
+                Z += term;
+                i++;
+            }
+            Console.WriteLine(String.Format("{0:0.0000}", Z));
+        }
+
+        static void doWhileCycle(double X, double Y, int N, double Z)
+        {
+            int i = 1;
+            do
+            {
+                double term;
+                double denominator = (2 * i - 1) * (2 * i + 1);
+
+                if (i % 2 != 0)
+                {
+                    term = Math.Pow(X, 2 * i) / denominator;
+                }
+                else
+                {
+                    term = -Math.Pow(Y, 2 * i) / denominator;
+                }
+
+                Z += term;
+                i++;
+            } while (i <= N);
+            Console.WriteLine(String.Format("{0:0.0000}", Z));
+        }
+    }
+}
